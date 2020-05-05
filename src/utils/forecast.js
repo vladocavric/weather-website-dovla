1bd88b57805dfb0ccbd673723b19883f9dde4310
vladocavric <32539882+vladocavric@users.noreply.github.com>
2020-05-05 13:03:14 +0200

    added text to about page

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,6 @@ const request = require('request');
 const owapikey = process.env.OWAPIKEY;
 const forecast = (lat, lon, callback) =>{
     const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${owapikey}`;
-    console.log(url)
     request({url, json: true}, (err, res) => {
         if(err){
             callback(err, undefined)
